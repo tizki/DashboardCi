1b7b39023e7f3e82ca0eb923208ecdbd77b9d783
oak <k.o.alon@gmail.com>
2015-03-18 16:50:32 +0200

    basic admin template

---
+++ src/main/webapp/chart.js
@@ -0,0 +1,52 @@
+    
+    // Load the Visualization API and the piechart package.
+    google.load('visualization', '1', {'packages':['corechart']});
+      
+    // Set a callback to run when the Google Visualization API is loaded.
+    google.setOnLoadCallback(drawChart);
+      
+    function drawChart() {
+      var jsonData = $.ajax({
+          url: "/distribution",
+          dataType:"json",
+          async: false
+          }).responseText;
+          
+var data = new google.visualization.DataTable();
+
+// Declare columns
+data.addColumn('string', 'Build Result');
+data.addColumn('number','Percentage');
+
+var result = JSON.parse(jsonData);
+for (var p in result){
+	 var ar = [];
+	ar.push(p)
+	ar.push(result[p]);
+        data.addRow(ar);
+}
+
+
+var getTimeToFix = function() {
+        var jsonData = $.ajax({
+            url: "/timeToFix",
+            dataType:"json",
+            async: false
+        }).responseJSON; return "Average Time To Fix" + jsonData.timeToFix   }
+
+$(document).ready(function() {
+                $("#timeToFix").text(getTimeToFix())
+            }
+    )
+
+      // Create our data table out of JSON data loaded from server.
+     // var data = new google.visualization.DataTable(jsonData);
+      var options = {
+            title: 'Builds Result Distribution ',
+            colors: ['#B0DF00', '#D52D2C', '#A6A6A6','#FDE06A']
+        };
+      // Instantiate and draw our chart, passing in some options.
+      var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+      chart.draw(data, options);
+    }
+
